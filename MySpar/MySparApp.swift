80bd543c2953df85df681668e4d2045bac2b1e2a
ivr88 <114075652+ivr88@users.noreply.github.com>
2024-01-30 21:40:43 +0300

    Initial Commit

---
+++ MySpar/MySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparApp.swift
+//  MySpar
+//
+//  Created by Вадим Исламов on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
